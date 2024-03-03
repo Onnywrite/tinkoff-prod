0c61c6cdcb0a05e362ab2e5b380a9dcbf4f3210d
Onnywrite <vlad.mokeev.061611@gmail.com>
2024-03-03 18:13:29 -0500

    Updated interface (Countries method)

--- solution/internal/storage/storage.go
+++ solution/internal/storage/storage.go
@@ -1,11 +1,16 @@
 package storage
 
-import "errors"
+import (
+	"errors"
+
+	"solution/internal/models"
+)
 
 var (
 	ErrCountryNotFound = errors.New("country not found")
 )
 
 type Storage interface {
+	Countries(regions ...string) ([]models.Country, error)
 	Disconnect() error
 }
