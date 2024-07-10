cc87df7e0771af61d638b5a529771f34235d41cb
Onnywrite <vlad.mokeev161106@gmail.com>
2024-07-10 21:49:20 +0300

    feat: added ID field for country model

--- internal/models/country.go
+++ internal/models/country.go
@@ -1,8 +1,9 @@
 package models
 
 type Country struct {
-	Name   string `field:"name" json:"name"`
-	Alpha2 string `field:"alpha2" json:"alpha2"`
-	Alpha3 string `field:"alpha3" json:"alpha3"`
-	Region string `field:"region" json:"region"`
+	Id     uint64 `json:"id"`
+	Name   string `json:"name"`
+	Alpha2 string `json:"alpha2"`
+	Alpha3 string `json:"alpha3"`
+	Region string `json:"region"`
 }
