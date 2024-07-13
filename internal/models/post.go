7fe9b8500afcb41ce3c9a0111e688ddd8ed51a08
Onnywrite <vlad.mokeev161106@gmail.com>
2024-07-13 22:29:53 +0300

    feat: added postgres array scanning

--- internal/models/post.go
+++ internal/models/post.go
@@ -1,12 +1,32 @@
 package models
 
-import "time"
+import (
+	"fmt"
+	"strings"
+	"time"
+)
 
 type Post struct {
-	Id          uint64     `json:"id"`
-	Author      User       `json:"author"`
-	Content     string     `json:"content"`
-	ImagesUrls  *[]string  `json:"images_urls"`
-	PublishedAt time.Time  `json:"published_at"`
-	UpdatedAt   *time.Time `json:"updated_at"`
+	Id          uint64      `json:"id"`
+	Author      User        `json:"author"`
+	Content     string      `json:"content"`
+	ImagesUrls  StringSlice `json:"images_urls"`
+	PublishedAt time.Time   `json:"published_at"`
+	UpdatedAt   *time.Time  `json:"updated_at"`
+}
+
+type StringSlice []string
+
+func (s *StringSlice) Scan(src interface{}) error {
+	if src == nil {
+		s = nil
+		return nil
+	}
+	str, ok := src.(string)
+	if !ok {
+		return fmt.Errorf("failed to scan images_urls")
+	}
+	str = strings.Trim(str, "{} ")
+	*s = strings.Split(str, ",")
+	return nil
 }
