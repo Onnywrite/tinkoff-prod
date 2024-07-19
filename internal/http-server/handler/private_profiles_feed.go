86cd17a831cd1a475c3e5b8f86422b251a089330
Onnywrite <vlad.mokeev161106@gmail.com>
2024-07-19 23:22:38 +0300

    feat: added profiles feed

---
+++ internal/http-server/handler/private_profiles_feed.go
@@ -0,0 +1,59 @@
+package handler
+
+import (
+	"context"
+	"errors"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/Onnywrite/tinkoff-prod/internal/services/feed"
+	"github.com/Onnywrite/tinkoff-prod/pkg/ero"
+	"github.com/labstack/echo/v4"
+)
+
+type AuthorFeedProvider interface {
+	AuthorFeed(ctx context.Context, page, pageSize uint64, userId uint64, formatDate func(time.Time) string) (*feed.PagedProfileFeed, ero.Error)
+}
+
+func GetProfileFeed(provider AuthorFeedProvider) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		idStr := strings.TrimPrefix(c.Param("id"), "id")
+		id, err := strconv.ParseUint(idStr, 10, 64)
+		if err != nil {
+			c.JSONBlob(http.StatusNotFound, errorMessage("id is not integer").Blob())
+			return err
+		}
+		page, err := strconv.ParseUint(c.QueryParam("page"), 10, 32)
+		if err != nil || page < 1 {
+			page = 1
+		}
+		pageSize, err := strconv.ParseUint(c.QueryParam("page_size"), 10, 32)
+		if err != nil || pageSize < 1 {
+			pageSize = 100
+		}
+		fullTimestamp, err := strconv.ParseBool(c.QueryParam("full_timestamp"))
+		if err != nil {
+			fullTimestamp = false
+		}
+
+		posts, eroErr := provider.AuthorFeed(context.Background(), page, pageSize, id, func(t time.Time) string {
+			if fullTimestamp {
+				return t.Format(time.DateTime)
+			} else {
+				return t.Format(time.DateOnly)
+			}
+		})
+		switch {
+		case errors.Is(eroErr, feed.ErrNoPosts):
+			c.JSONBlob(http.StatusNoContent, []byte(eroErr.Error()))
+			return eroErr
+		case eroErr != nil:
+			c.JSONBlob(http.StatusInternalServerError, []byte(eroErr.Error()))
+			return eroErr
+		}
+
+		return c.JSON(http.StatusOK, posts)
+	}
+}
