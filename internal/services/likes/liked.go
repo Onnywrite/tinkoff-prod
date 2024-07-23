c458e735b4b37a2ecadec01e0faa4c456a70e12b
Onnywrite <vlad.mokeev161106@gmail.com>
2024-07-23 17:48:19 +0300

    fix: a lot of errors

--- internal/services/likes/liked.go
+++ internal/services/likes/liked.go
@@ -2,13 +2,20 @@ package likes
 
 import (
 	"context"
+	"errors"
+
+	"github.com/Onnywrite/tinkoff-prod/internal/storage"
 )
 
 func (s *Service) IsLiked(ctx context.Context, userId, postId uint64) bool {
 	// logCtx := erolog.NewContextBuilder().With("op", "likes.Service.IsLiked").With("user_id", userId).With("post_id", postId)
 
 	_, err := s.d.LikeProvider.Like(ctx, userId, postId)
-	if err != nil {
+	switch {
+	case errors.Is(err, storage.ErrNoRows):
+		s.log.DebugContext(err.Context(ctx), "no likes")
+		return false
+	case err != nil:
 		s.log.ErrorContext(err.Context(ctx), "error while getting like")
 		return false
 	}
